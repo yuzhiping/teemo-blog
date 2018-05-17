3b261c01c368f7d1d9a1d2477ff07b84e9e98603
yuzhiping <yuzhiping140103@hotmail.com>
2018-05-17 23:04:13 +0800

    初始化项目

---
+++ src/main/java/com/github/hexsmith/blog/TeemoBlogApplication.java
@@ -0,0 +1,17 @@
+package com.github.hexsmith.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hexsmith
+ * @since 2018-05-17 22:57
+ * @version v1.0
+ */
+@SpringBootApplication
+public class TeemoBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeemoBlogApplication.class, args);
+    }
+}
